7fb34ef803ae62e58bb5f27eb8039d834714c490
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2024-03-31 22:14:58 -0300

    Create Arquivo.cs

---
+++ Vetor/Arquivo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fila-vetor
+{
+    internal class Arquivo
+    {
+        private string nome;
+        private int paginas;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int Paginas
+        {
+            get { return paginas; }
+            set { paginas = value; }
+        }
+        public Arquivo(string nome, int paginas)
+        {
+            this.nome = nome;
+            this.paginas = paginas;
+        }
+    }
+}
